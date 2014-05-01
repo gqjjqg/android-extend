a26c9456e4306eb281f44722c099fa325a36ccf1
gqjjqg <qijiang.guo@gmail.com>
2014-05-02 00:37:41 +0800

    add ignore. Signed-off-by: gqjjqg <qijiang.guo@gmail.com>

--- android-widget-extend/gen/com/guo/android_widget_externed/BuildConfig.java
+++ android-widget-extend/gen/com/guo/android_widget_externed/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.sample;
+package com.guo.android_widget_externed;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- android-widget-extend/gen/com/guo/android_widget_externed/R.java
+++ android-widget-extend/gen/com/guo/android_widget_externed/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.sample;
+package com.guo.android_widget_externed;
 
 public final class R {
     public static final class attr {
@@ -16,7 +16,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int itemCenter=0x7f010002;
+        public static int itemCenter=0x7f010002;
         /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
 Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
 in (inches), mm (millimeters).
@@ -26,7 +26,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int itemOffsetLeft=0x7f010001;
+        public static int itemOffsetLeft=0x7f010001;
         /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
 Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
 in (inches), mm (millimeters).
@@ -36,7 +36,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int itemOffsetTop=0x7f010000;
+        public static int itemOffsetTop=0x7f010000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -44,33 +44,17 @@ containing a value of this type.
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
- Default screen margins, per the Android Design guidelines. 
-
-         Example customization of dimensions originally defined in res/values/dimens.xml
-         (such as screen margins) for screens with more than 820dp of available width. This
-         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
-    
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static int activity_horizontal_margin=0x7f030000;
+        public static int activity_vertical_margin=0x7f030001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-    }
-    public static final class id {
-        public static final int container=0x7f070000;
-        public static final int imageView1=0x7f070002;
-        public static final int listView1=0x7f070001;
-        public static final int textView1=0x7f070003;
-    }
-    public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int item_sample=0x7f030001;
+        public static int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static int action_settings=0x7f040001;
+        public static int app_name=0x7f040000;
+        public static int hello_world=0x7f040002;
     }
     public static final class style {
         /** 
@@ -82,23 +66,12 @@ containing a value of this type.
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static int AppTheme=0x7f050001;
     }
     public static final class styleable {
         /**  attribute for HorizontalListView. 
@@ -107,9 +80,9 @@ containing a value of this type.
            <colgroup align="left" />
            <colgroup align="left" />
            <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #HorizontalListView_itemCenter com.example.sample:itemCenter}</code></td><td></td></tr>
-           <tr><td><code>{@link #HorizontalListView_itemOffsetLeft com.example.sample:itemOffsetLeft}</code></td><td></td></tr>
-           <tr><td><code>{@link #HorizontalListView_itemOffsetTop com.example.sample:itemOffsetTop}</code></td><td></td></tr>
+           <tr><td><code>{@link #HorizontalListView_itemCenter com.guo.android_widget_externed:itemCenter}</code></td><td></td></tr>
+           <tr><td><code>{@link #HorizontalListView_itemOffsetLeft com.guo.android_widget_externed:itemOffsetLeft}</code></td><td></td></tr>
+           <tr><td><code>{@link #HorizontalListView_itemOffsetTop com.guo.android_widget_externed:itemOffsetTop}</code></td><td></td></tr>
            </table>
            @see #HorizontalListView_itemCenter
            @see #HorizontalListView_itemOffsetLeft
@@ -119,7 +92,7 @@ containing a value of this type.
             0x7f010000, 0x7f010001, 0x7f010002
         };
         /**
-          <p>This symbol is the offset where the {@link com.example.sample.R.attr#itemCenter}
+          <p>This symbol is the offset where the {@link com.guo.android_widget_externed.R.attr#itemCenter}
           attribute's value can be found in the {@link #HorizontalListView} array.
 
 
@@ -129,11 +102,11 @@ containing a value of this type.
 theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
-          @attr name com.example.sample:itemCenter
+          @attr name com.guo.android_widget_externed:itemCenter
         */
         public static final int HorizontalListView_itemCenter = 2;
         /**
-          <p>This symbol is the offset where the {@link com.example.sample.R.attr#itemOffsetLeft}
+          <p>This symbol is the offset where the {@link com.guo.android_widget_externed.R.attr#itemOffsetLeft}
           attribute's value can be found in the {@link #HorizontalListView} array.
 
 
@@ -145,11 +118,11 @@ in (inches), mm (millimeters).
 theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
-          @attr name com.example.sample:itemOffsetLeft
+          @attr name com.guo.android_widget_externed:itemOffsetLeft
         */
         public static final int HorizontalListView_itemOffsetLeft = 1;
         /**
-          <p>This symbol is the offset where the {@link com.example.sample.R.attr#itemOffsetTop}
+          <p>This symbol is the offset where the {@link com.guo.android_widget_externed.R.attr#itemOffsetTop}
           attribute's value can be found in the {@link #HorizontalListView} array.
 
 
@@ -161,7 +134,7 @@ in (inches), mm (millimeters).
 theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
-          @attr name com.example.sample:itemOffsetTop
+          @attr name com.guo.android_widget_externed:itemOffsetTop
         */
         public static final int HorizontalListView_itemOffsetTop = 0;
     };
