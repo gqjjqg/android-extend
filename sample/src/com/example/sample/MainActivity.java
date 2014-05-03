33ffc6c0e40bd68ab434c3569c724d9c858e7a1a
gqjjqg <qijiang.guo@gmail.com>
2014-05-03 19:34:02 +0800

    set scale layout. Signed-off-by: gqjjqg <qijiang.guo@gmail.com>

--- sample/src/com/example/sample/MainActivity.java
+++ sample/src/com/example/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sample;
 
 import com.guo.android_extend.CustomOrientationDetector;
 import com.guo.android_extend.CustomOrientationDetector.OnOrientationListener;
+import com.guo.android_extend.widget.ExtRelativeLayout;
 import com.guo.android_extend.widget.HorizontalListView;
 import com.guo.android_extend.widget.HorizontalListView.OnItemScrollListener;
 import com.guo.android_extend.widget.ExtImageView;
@@ -44,7 +45,7 @@ public class MainActivity extends Activity {
 		ViewList2Adapter vla2 = new ViewList2Adapter(this);
 		HorizontalListView hv2 = (HorizontalListView) this.findViewById(R.id.listView2);
 		hv2.setAdapter(vla2);
-		hv2.setOnItemScrollListener(new HScrollListener());
+		hv2.setOnItemScrollListener(new HScrollListener2());
 		hv2.setOnItemClickListener(vla2);
 		
 		mODetector = new CustomOrientationDetector(this);
@@ -55,6 +56,12 @@ public class MainActivity extends Activity {
 		Holder holder = (Holder) v.getTag();
 		holder.siv.setScale(percent, percent);
 		holder.siv.invalidate();
+	}
+	
+	private void scale2(View v, float percent) {
+		ExtRelativeLayout ert = (ExtRelativeLayout) v;
+		ert.setScale(percent, percent);
+		ert.invalidate();
 	}
 	
 	class Holder {
@@ -187,7 +194,7 @@ public class MainActivity extends Activity {
 			convertView.setWillNotDraw(false);
 			mODetector.addReceiver((OnOrientationListener) convertView);
 			
-			scale(convertView, 1f - percent_add);
+			scale2(convertView, 1f - percent_add);
 			
 			return convertView;
 		}
@@ -216,6 +223,54 @@ public class MainActivity extends Activity {
 			}
 			if (pos > 0) {
 				scale(adp.getChildAt(pos - 1), 1f - percent_add);
+			}
+			
+		}
+
+		@Override
+		public void OnScrollStart(AdapterView<ListAdapter> adp) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "OnScrollStart");
+		}
+
+		@Override
+		public void OnScrollEnd(AdapterView<ListAdapter> adp, int pos) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "OnScrollEnd pos=" + pos);
+			mODetector.forceOrientationChanged();
+		}
+
+		@Override
+		public boolean OnDraging(AdapterView<ListAdapter> adp, float dx,
+				float dy) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "OnDraging dx=" + dx + ", dy=" + dy);
+			return false;
+		}
+
+		@Override
+		public boolean OnDragingOver(AdapterView<ListAdapter> adp) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "OnDragingOver");
+			return true;
+		}
+		
+	}
+	
+	class HScrollListener2 implements OnItemScrollListener {
+
+		@Override
+		public void OnScrollCenter(AdapterView<ListAdapter> adp, View v,
+				int pos, float percent) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "OnScrollCenter pos=" + pos + ", percent=" + percent);
+			float scale = (1f - percent_add) + percent_add * percent;
+			scale2(v, scale);
+			if (adp.getChildCount() > pos + 1) {
+				scale2(adp.getChildAt(pos + 1), 1f + percent_add - scale);
+			}
+			if (pos > 0) {
+				scale2(adp.getChildAt(pos - 1), 1f - percent_add);
 			}
 			
 		}
