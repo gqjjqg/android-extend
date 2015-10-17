a9cd8f8c33041934a87c628c8d8f4dc9bae4571d
qijiang.guo <qijiang.guo@gmail.com>
2015-10-17 23:44:12 +0800

    Signed-off-by: qijiang.guo <qijiang.guo@gmail.com>

--- src/main/src/com/guo/android_extend/widget/CameraSurfaceView.java
+++ src/main/src/com/guo/android_extend/widget/CameraSurfaceView.java
@@ -1,5 +1,7 @@
 package com.guo.android_extend.widget;
 
+import com.guo.android_extend.GLES2Render;
+import com.guo.android_extend.image.ImageConverter;
 import com.guo.android_extend.tools.FrameHelper;
 
 import android.content.Context;
@@ -7,29 +9,83 @@ import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.Size;
+import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 /**
  * @Note create by gqjjqg,.
  *    easy to use camera.
  */
-public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback, PreviewCallback {
+public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback, PreviewCallback, GLSurfaceView.Renderer {
 	private final String TAG = this.getClass().getSimpleName();
 
 	private Camera mCamera;
+	private int mWidth, mHeight, mFormat;
 	private OnCameraListener mOnCameraListener;
 	private FrameHelper mFrameHelper;
-	
+	private GLES2Render mGLES2Render;
+	private GLSurfaceView mGLSurfaceView;
+	private BlockingQueue<byte[]> mImageDataBuffers;
+	private BlockingQueue<byte[]> mImageRenderBuffers;
+
+	private int mDegree;
+	private boolean mAutofit;
+	private boolean mMirror;
+	private boolean mDebugFPS;
+
 	public interface OnCameraListener {
+		/**
+		 * setup camera.
+		 * @return
+		 */
 		public Camera setupCamera();
+
+		/**
+		 * reset on surfaceChanged.
+		 * @param format
+		 * @param width
+		 * @param height
+		 */
 		public void setupChanged(int format, int width, int height);
+
+		/**
+		 * start preview immediately, after surfaceCreated
+		 * @return
+		 */
 		public boolean startPreviewLater();
-		public void onPreview(byte[] data, Camera camera);
+
+		/**
+		 * on ui thread.
+		 * @param data
+		 * @param width
+		 * @param height
+		 * @param format
+		 */
+		public void onPreview(byte[] data, int width, int height, int format);
+
+		/**
+		 * on render thread.before render
+		 * @param data
+		 * @param width
+		 * @param height
+		 * @param format
+		 */
+		public void onPreviewRender(byte[] data, int width, int height, int format);
 	}
-	
+
 	public CameraSurfaceView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
@@ -52,10 +108,17 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
 		SurfaceHolder arg0 = getHolder();
 		arg0.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		arg0.addCallback(this);
-		
+
 		mFrameHelper = new FrameHelper();
+		mDebugFPS = false;
+		mFrameHelper.setShow(mDebugFPS);
+
+		mMirror = false;
+		mDegree = 0;
+		mImageDataBuffers = new LinkedBlockingQueue<>();
+		mImageRenderBuffers = new LinkedBlockingQueue<>();
 	}
-	
+
 	private boolean openCamera() {
 		try {
 			if (mCamera != null) {
@@ -65,15 +128,34 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
 					mCamera = mOnCameraListener.setupCamera();
 				}
 			}
-			
+
 			if (mCamera != null) {
 				mCamera.setPreviewDisplay(getHolder());
-				
+
 				Size imageSize = mCamera.getParameters().getPreviewSize();
-				int lineBytes = imageSize.width * ImageFormat.getBitsPerPixel(mCamera.getParameters().getPreviewFormat()) / 8;
-				mCamera.addCallbackBuffer(new byte[lineBytes * imageSize.height]);
-				mCamera.addCallbackBuffer(new byte[lineBytes * imageSize.height]);
-				mCamera.addCallbackBuffer(new byte[lineBytes * imageSize.height]);
+				mWidth = imageSize.width;
+				mHeight = imageSize.height;
+				mFormat = mCamera.getParameters().getPreviewFormat();
+
+				if (mAutofit) {
+					try {
+						ExtGLSurfaceView view = (ExtGLSurfaceView) mGLSurfaceView;
+						if (mDegree == 0 || mDegree == 180) {
+							view.setAspectRatio(mWidth, mHeight);
+						} else {
+							view.setAspectRatio(mHeight, mWidth);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				int lineBytes = imageSize.width * ImageFormat.getBitsPerPixel(mFormat) / 8;
+				mCamera.addCallbackBuffer(new byte[lineBytes * mHeight]);
+				mCamera.addCallbackBuffer(new byte[lineBytes * mHeight]);
+				mCamera.addCallbackBuffer(new byte[lineBytes * mHeight]);
+				mImageDataBuffers.add(new byte[lineBytes * mHeight]);
+				mImageDataBuffers.add(new byte[lineBytes * mHeight]);
+				mImageDataBuffers.add(new byte[lineBytes * mHeight]);
 				mCamera.setPreviewCallbackWithBuffer(this);
 				return true;
 			}
@@ -82,7 +164,7 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
 		}
 		return false;
 	}
-	
+
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		// TODO Auto-generated method stub
@@ -115,26 +197,81 @@ public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Call
 	        mCamera.release();
 	        mCamera = null;
 		}
+		mImageDataBuffers.clear();
 	}
 
 	@Override
 	public void onPreviewFrame(byte[] data, Camera camera) {
 		// TODO Auto-generated method stub
+		long time = System.currentTimeMillis();
 		mFrameHelper.printFPS();
-		
-		if (mOnCameraListener != null) {
-			mOnCameraListener.onPreview(data, camera);
+		byte[] buffer = mImageDataBuffers.poll();
+		if (buffer != null) {
+			System.arraycopy(data, 0, buffer, 0, buffer.length);
+			if (mOnCameraListener != null) {
+				mOnCameraListener.onPreview(buffer, mWidth, mHeight, mFormat);
+			}
+			if (!mImageRenderBuffers.offer(buffer)) {
+				Log.e(TAG, "RENDER QUEUE FULL!");
+			} else {
+				mGLSurfaceView.requestRender();
+			}
 		}
 		if (mCamera != null) {
 			mCamera.addCallbackBuffer(data);
+		}
+		Log.d(TAG, "onPreviewFrame COST=" + (System.currentTimeMillis() - time));
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		Camera.Parameters parameters = mCamera.getParameters();
+		int format = parameters.getPreviewFormat();
+		int convert = 0;
+		switch(format) {
+		case ImageFormat.NV21 : convert = ImageConverter.CP_PAF_NV21; break;
+		case ImageFormat.RGB_565 : convert = ImageConverter.CP_RGB565; break;
+		default: Log.e(TAG, "Current camera preview format = " + format + ", render is not support!");
+		}
+		mGLES2Render = new GLES2Render(mMirror, mDegree, convert, mDebugFPS);
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		mGLES2Render.setViewPort(width, height);
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		byte[] buffer = mImageRenderBuffers.poll();
+		if (buffer != null) {
+			if (mOnCameraListener != null) {
+				mOnCameraListener.onPreviewRender(buffer, mWidth, mHeight, mFormat);
+			}
+			mGLES2Render.render(buffer, mWidth, mHeight);
+			if (!mImageDataBuffers.offer(buffer)) {
+				Log.e(TAG, "PREVIEW QUEUE FULL!");
+			}
 		}
 	}
 
 	public void setOnCameraListener(OnCameraListener l) {
 		mOnCameraListener = l;
 	}
-	
-	public void showFPS(boolean open) {
+
+	public void setupGLSurafceView(GLSurfaceView glv, boolean autofit, boolean mirror, int degree) {
+		mGLSurfaceView = glv;
+		mGLSurfaceView.setEGLContextClientVersion(2);
+		mGLSurfaceView.setRenderer(this);
+		mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+		mGLSurfaceView.setZOrderMediaOverlay(true);
+		mDegree = degree;
+		mMirror = mirror;
+		mAutofit = autofit;
+	}
+
+	public void debug_print_fps(boolean open) {
+		//mDebugFPS = open;
 		mFrameHelper.setShow(open);
 	}
 }
