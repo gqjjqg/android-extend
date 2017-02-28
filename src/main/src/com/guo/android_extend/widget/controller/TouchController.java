18f11cfb7f2eadd2a7cc9153e25761883387bd75
gqjjqg <qijiang.guo@gmail.com>
2017-02-28 08:42:05 +0800

    support multiple finger Signed-off-by: gqjjqg <qijiang.guo@gmail.com>

--- src/main/src/com/guo/android_extend/widget/controller/TouchController.java
+++ src/main/src/com/guo/android_extend/widget/controller/TouchController.java
@@ -30,13 +30,33 @@ public class TouchController  {
 	}
 
 	public MotionEvent obtainTouchEvent(MotionEvent ev, int view_w, int view_h, int cur_degree) {
-		PointF newPoint = rotatePoint(new PointF(ev.getX(), ev.getY()),
-				new PointF(view_w / 2F, view_h / 2F), -cur_degree);
-		MotionEvent newEvent = MotionEvent.obtain(ev.getDownTime(),
-				ev.getEventTime(), ev.getAction(), newPoint.x, newPoint.y,
-				ev.getPressure(), ev.getSize(), ev.getMetaState(),
-				ev.getXPrecision(), ev.getYPrecision(), ev.getDeviceId(),
-				ev.getEdgeFlags());
+		MotionEvent newEvent;
+		if (false) {	// suport one touch
+			PointF newPoint = rotatePoint(new PointF(ev.getX(), ev.getY()),
+					new PointF(view_w / 2F, view_h / 2F), -cur_degree);
+			newEvent = MotionEvent.obtain(ev.getDownTime(),
+					ev.getEventTime(), ev.getAction(), newPoint.x, newPoint.y,
+					ev.getPressure(), ev.getSize(), ev.getMetaState(),
+					ev.getXPrecision(), ev.getYPrecision(), ev.getDeviceId(),
+					ev.getEdgeFlags());
+		} else {
+			MotionEvent.PointerProperties[] preo = new MotionEvent.PointerProperties[2];
+			MotionEvent.PointerCoords[] coor = new MotionEvent.PointerCoords[2];
+			for (int i = 0; i < ev.getPointerCount(); i++) {
+				preo[i] = new MotionEvent.PointerProperties();
+				coor[i] = new MotionEvent.PointerCoords();
+				ev.getPointerProperties(i, preo[i]);
+				ev.getPointerCoords(i, coor[i]);
+				PointF newPoint = rotatePoint(new PointF(coor[i].x, coor[i].y),
+						new PointF(view_w / 2F, view_h / 2F), -cur_degree);
+				coor[i].x = newPoint.x;
+				coor[i].y = newPoint.y;
+			}
+			newEvent = MotionEvent.obtain(ev.getDownTime(), ev.getEventTime(), ev.getAction(), ev.getPointerCount(), preo,
+					coor, ev.getMetaState(), ev.getButtonState(), ev.getXPrecision(), ev.getYPrecision(), ev.getDeviceId(),
+					ev.getEdgeFlags(), ev.getSource(), ev.getFlags());
+		}
+
 		return newEvent;
 	}
 }
