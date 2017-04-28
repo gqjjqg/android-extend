8b76e8c65b77638f51b25eb62d2adb424e0d0d25
gqjjqg <qijiang.guo@gmail.com>
2017-04-28 15:31:04 +0800

    add Signed-off-by: gqjjqg <qijiang.guo@gmail.com>

--- src/main/src/com/guo/android_extend/java/ExtByteArrayOutputStream.java
+++ src/main/src/com/guo/android_extend/java/ExtByteArrayOutputStream.java
@@ -1,10 +1,6 @@
 package com.guo.android_extend.java;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 
 /**
  * Created by Guo on 2015/12/26.
@@ -22,4 +18,5 @@ public class ExtByteArrayOutputStream extends ByteArrayOutputStream {
     public byte[] getByteArray() {
         return super.buf;
     }
+
 }
